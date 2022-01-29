338405c0983f2895f5d491498ef03b10d34daa6a
Luciano dii souza <lucianodiisouza@hotmail.com>
2022-01-29 12:07:44 -0300

    chore: add react-markdown/wip: user posts ISR page

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Loader } from "./Loader";
 export { default as UserProfile } from "./UserProfile";
 export { default as PostFeed } from "./PostFeed";
+export { default as PostContent } from "./PostContent";
