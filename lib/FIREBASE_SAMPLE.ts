69b7db15c037b4536d71e875895fdfeac48a8e29
Luciano dii souza <luciano.souza@tc.com.br>
2022-01-28 22:34:29 -0300

    feat: initialize firebase

--- lib/FIREBASE_SAMPLE.ts
+++ lib/FIREBASE_SAMPLE.ts
@@ -3,8 +3,22 @@ import { getApps, getApp, initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "XXXXXXXXXX",
+  authDomain: "XXXXXXXXXX",
+  projectId: "XXXXXXXXXX",
+  storageBucket: "XXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXX",
+  appId: "XXXXXXXXXX",
+  measurementId: "XXXXXXXXXX",
+};
+
 const firebase = getApps().length ? getApp() : initializeApp(firebaseConfig);
 
 export const auth = getAuth(firebase);
 export const firestore = getFirestore(firebase);
 export const storage = getStorage(firebase);
+
+/** Rename this file to firebase.ts
+ *  paste your firebase config keys at firebaseConfig const
+ */
