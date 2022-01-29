0995fc68bbb28157de1b55e12af1c58c2542efca
Luciano dii souza <luciano.souza@tc.com.br>
2022-01-29 11:27:59 -0300

    feat: update FIREBASE_SAMPLE file

--- lib/FIREBASE_SAMPLE.ts
+++ lib/FIREBASE_SAMPLE.ts
@@ -23,6 +23,26 @@ export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
 
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+
+export async function getUserWithUsername(username: string) {
+  const usersRef = firestore.collection("users");
+  const query = usersRef.where("username", "==", username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+
+  return userDoc;
+}
+
+export function postToJson(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis(),
+  };
+}
+
+
 
 /** Rename this file to firebase.ts
  *  paste your firebase config keys at firebaseConfig const
