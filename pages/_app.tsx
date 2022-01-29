15638e547803321a7ba1faefd9c44724737874e8
Luciano dii souza <luciano.souza@tc.com.br>
2022-01-28 23:43:23 -0300

    chore: adding react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "components";
+import { Toaster } from "react-hot-toast";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -6,6 +7,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import { Loader } from "components";
+import toast from "react-hot-toast";
 
 export default function Home() {
   return (
     <div>
       <Loader show={false} />
+      <button onClick={() => toast.success("Really easy!")}>Toast</button>
     </div>
   );
 }
