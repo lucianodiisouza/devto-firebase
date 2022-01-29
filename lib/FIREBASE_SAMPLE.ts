d92ce07ba417433d6333273cdb67fc38f967a073
Luciano dii souza <luciano.souza@tc.com.br>
2022-01-29 01:03:34 -0300

    fix: updaing FIREBASE_SAMPLE.ts file

--- lib/FIREBASE_SAMPLE.ts
+++ lib/FIREBASE_SAMPLE.ts
@@ -18,8 +18,11 @@ if (!firebase.apps.length) {
 }
 
 export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+
 
 /** Rename this file to firebase.ts
  *  paste your firebase config keys at firebaseConfig const
