16d4a8c6085b06a9cba5c57028bfff84bdceda38
Luciano dii souza <luciano.souza@tc.com.br>
2022-01-29 11:23:59 -0300

    feat: fetch and display firebase posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,66 @@
-import { Loader } from "components";
-import toast from "react-hot-toast";
+import { useState } from "react";
 
-export default function Home() {
+import { Loader, PostFeed } from "components";
+import { firestore, fromMillis, postToJson } from "lib/firebase";
+
+const LIMIT = 1;
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+
+    const last = posts && posts[posts.length - 1];
+
+    const cursor =
+      last && typeof last.createdAt === "number"
+        ? fromMillis(last.createdAt)
+        : last.createdAt;
+
+    const query = firestore
+      .collectionGroup("posts")
+      .where("published", "==", true)
+      .orderBy("createdAt", "desc")
+      .startAfter(cursor)
+      .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+
+    setLoading(false);
+
+    if (newPosts.length < LIMIT) {
+      setPostsEnd(true);
+    }
+  };
+
   return (
-    <div>
-      <Loader show={false} />
-      <button onClick={() => toast.success("Really easy!")}>Toast</button>
-    </div>
+    <main>
+      <PostFeed posts={posts} />
+      {posts.length === 0 && "Nothing to see yet 😅"}
+      {!loading && !postsEnd && (
+        <button onClick={getMorePosts}>Load more</button>
+      )}
+    </main>
   );
+}
+
+export async function getServerSideProps() {
+  const postsQuery = firestore
+    .collectionGroup("posts")
+    .where("published", "==", true)
+    .orderBy("createdAt", "desc")
+    .limit(LIMIT);
+
+  const posts = (await postsQuery.get()).docs.map(postToJson);
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
