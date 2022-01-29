cf27bf23e9514120268bebbf4528bc8e9bc42afe
Luciano dii souza <lucianodiisouza@hotmail.com>
2022-01-29 12:00:48 -0300

    wip: display user posts using ISR

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,7 +1,50 @@
+import { firestore, getUserWithUsername, postToJson } from "lib/firebase";
+
 export default function PostPage({}) {
   return (
     <main>
       <h1>Postpage</h1>
     </main>
   );
+}
+
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+
+  const userDoc = await getUserWithUsername(username);
+  let post;
+  let path;
+
+  if (userDoc) {
+    const postRef = userDoc.ref.collection("posts").doc(slug);
+
+    post = postToJson(await postRef.get());
+
+    path = postRef.path;
+  }
+
+  return {
+    props: {
+      post,
+      path,
+    },
+    revalidate: 5000,
+  };
+}
+
+export async function getStaticPaths() {
+  const snapshot = await firestore.collectionGroup("posts").get();
+
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
+
+    return {
+      params: { username, slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
 }
