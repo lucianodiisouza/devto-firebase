a68381e14aeac24266e1bfc8d10f983036ac2f5f
Luciano dii souza <luciano.souza@tc.com.br>
2022-01-29 01:39:26 -0300

    wip: username choosen form

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,7 @@
-import { useContext } from "react";
-
+import { FormEvent, useCallback, useContext, useEffect, useState } from "react";
+import debounce from "lodash.debounce";
 import { UserContext } from "contexts";
-import { auth, googleAuthProvider } from "lib/firebase";
+import { auth, googleAuthProvider, firestore } from "lib/firebase";
 
 export default function Enter({}) {
   const { user, username } = useContext(UserContext);
@@ -22,13 +22,14 @@ export default function Enter({}) {
 }
 
 const SignInButton = () => {
-  const signInWithGoogle = () => {
-    auth
-      .signInWithPopup(googleAuthProvider)
-      .then((res) => console.log(res))
-      .catch((err) => console.log(err));
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
   };
-  return <button onClick={signInWithGoogle}>Login com Google</button>;
+  return (
+    <button onClick={signInWithGoogle} className="btn-google">
+      Login com Google
+    </button>
+  );
 };
 
 const SignOutButton = () => {
@@ -36,9 +37,106 @@ const SignOutButton = () => {
 };
 
 const UserNameForm = () => {
+  const [formValue, setFormValue] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { user, username } = useContext(UserContext);
+
+  const onChange = (e) => {
+    const val = e.target.value.toLowerCase();
+    const re = /^(?=[a-zA-Z0-9._]{3,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
+
+    if (val.length < 3) {
+      setFormValue(val);
+      setLoading(false);
+      setIsValid(false);
+    }
+
+    if (re.test(val)) {
+      setFormValue(val);
+      setLoading(true);
+      setIsValid(false);
+    }
+  };
+
+  const checkUsername = useCallback(
+    debounce(async (username) => {
+      if (username.length >= 3) {
+        const ref = firestore.doc(`usernames/${username}`);
+        const { exists } = await ref.get();
+        setIsValid(!exists);
+        setLoading(false);
+      }
+    }, 500),
+    []
+  );
+
+  const onSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const userDoc = firestore.doc(`users/$user.uid`);
+    const usernameDoc = firestore.doc(`usernames/${formValue}`);
+
+    const batch = firestore.batch();
+
+    batch.set(userDoc, {
+      username: formValue,
+      photoURL: user.photoURL,
+      displayName: user.displayName,
+    });
+    batch.set(usernameDoc, { uid: user.uid });
+
+    await batch.commit();
+  };
+
+  useEffect(() => {
+    checkUsername(formValue);
+  }, [formValue, checkUsername]);
+
   return (
-    <form>
-      <input type="text" />
-    </form>
+    !username && (
+      <section>
+        <h3>Choose your username</h3>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="type your username"
+            value={formValue}
+            onChange={onChange}
+          />
+
+          <UsernameMessage
+            username={formValue}
+            isValid={isValid}
+            loading={loading}
+          />
+
+          <button type="submit" className="btn-green" disabled={!isValid}>
+            Choose
+          </button>
+
+          <h3>DebugState</h3>
+          <div>
+            UserName: {formValue} <br />
+            Loading: {loading.toString()} <br />
+            Username Valid: {isValid.toString()}
+          </div>
+        </form>
+      </section>
+    )
   );
 };
+
+function UsernameMessage({ username, isValid, loading }) {
+  if (loading) {
+    return <p>Checking ...</p>;
+  } else if (isValid) {
+    return <p className="text-success">{username} is available!</p>;
+  } else if (username && !isValid) {
+    return <p className="text-danger">That username is already taken!</p>;
+  } else {
+    return <p></p>;
+  }
+}
